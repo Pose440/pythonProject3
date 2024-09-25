1e2b679ddaf9f997112f5b8c9b7d5970a35974da
Pose440 <oksana.agrba@mail.ru>
2024-09-25 19:26:00 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+numbers = print("Введите 3 числа:")
+first = int(input())
+second = int(input())
+third = int(input())
+if first == third == second:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
