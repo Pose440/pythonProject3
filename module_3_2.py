d6d2ba448f1364df96ad1f6835ab2914b40fe3f3
Pose440 <oksana.agrba@mail.ru>
2024-09-30 22:16:40 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,14 +1,14 @@
-def send_email(message, recipient, sender="university.help@gmail.com" ):
-   if  "@" not in str((recipient, sender)) or recipient.endswith(".com" or ".ru" or ".net") or sender.endswith(".com" or ".ru" or ".net"):
+def send_email(message, recipient,*, sender = "university.help@gmail.com"):
+   if  "@" not in str(recipient) or "@" not in str(sender) or "recipient".endswith((".com", ".ru", ".net")) or "sender".endswith((".com",".ru",".net")):
       print("Невозможно отправить письмо с адреса" ,sender, "на адрес" ,recipient)
    elif sender == recipient:
        print("Нельзя отправить письмо самому себе!")
    elif sender == "university.help@gmail.com":
        print("Письмо успешно отправлено с адреса" ,sender, "на адрес" ,recipient)
-   else:
+   elif sender != "university.help@gmail.com":
        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса" ,sender, "на адрес" ,recipient)
 
-send_email("Привет", " vasyok1337@gmail.com")
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender = 'urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender = 'urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender = 'urban.teacher@mail.ru')
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
