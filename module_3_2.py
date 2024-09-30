c4bed94533db4f7791277b6ca93f5a448274cb27
Pose440 <oksana.agrba@mail.ru>
2024-09-30 23:32:41 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,11 +1,13 @@
-def send_email(message, recipient,*, sender = "university.help@gmail.com"):
-   if  "@" not in str(recipient) or "@" not in str(sender) or "recipient".endswith((".com", ".ru", ".net")) or "sender".endswith((".com",".ru",".net")):
+def send_email(message, recipient,*, sender="university.help@gmail.com"):
+   if  ("@" not in recipient or "@" not in sender) or (not recipient.endswith((".com", ".ru", ".net")) or not sender.endswith((".com",".ru",".net"))):
       print("Невозможно отправить письмо с адреса" ,sender, "на адрес" ,recipient)
+      return
    elif sender == recipient:
        print("Нельзя отправить письмо самому себе!")
+       return
    elif sender == "university.help@gmail.com":
        print("Письмо успешно отправлено с адреса" ,sender, "на адрес" ,recipient)
-   elif sender != "university.help@gmail.com":
+   else:
        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса" ,sender, "на адрес" ,recipient)
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
