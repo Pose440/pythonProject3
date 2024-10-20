5dce22a2c3e6953538086f57374a35521f064baf
Pose440 <oksana.agrba@mail.ru>
2024-10-20 08:22:19 +0300

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -21,23 +21,23 @@ class House:
         return self.number_of_floors == other
 
     def __lt__(self, other):
-        if isinstance(other.number_of_floors, int):
+        if isinstance(other, House):
             return self.number_of_floors < other.number_of_floors
 
     def __le__(self, other):
-        if isinstance(other.number_of_floors, int):
+        if isinstance(other, House):
             return self.number_of_floors <= other.number_of_floors
 
     def __gt__(self, other):
-        if isinstance(other.number_of_floors, int):
+        if isinstance(other, House):
             return self.number_of_floors > other.number_of_floors
 
     def __ge__(self, other):
-        if isinstance(other.number_of_floors, int):
+        if isinstance(other, House):
             return self.number_of_floors >= other.number_of_floors
 
     def __ne__(self, other):
-        if isinstance(other.number_of_floors, int):
+        if isinstance(other, House):
            return self.number_of_floors != other.number_of_floors
 
     def __add__(self, value):
@@ -47,8 +47,7 @@ class House:
 
     def __iadd__(self, value):
         if isinstance(value, int):
-            self.number_of_floors += value
-        return self
+            return self.__add__(value)
 
     def __radd__(self, value):
         if isinstance(value, int):
