e8ea360f53f6ae1d2b3ef3308d3b1040a6312bda
Pose440 <oksana.agrba@mail.ru>
2024-11-17 21:06:38 +0300

    Add files via upload

---
+++ module_7_4.py
@@ -0,0 +1,23 @@
+tasks_total = 82
+time_avg = 45.2
+
+print(f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!")
+
+score_1 = 40
+score_2 = 42
+challenge_result = "победа команды Волшебники данных!"
+
+print(f"Команды решили {score_1} и {score_2} задач.")
+print(f"Результат битвы: {challenge_result}")
+
+score_2 = 42
+team1_time = 1552.512
+
+print("Команда Волшебники данных решила задач: {} !".format(score_2))
+print("Волшебники данных решили задачи за {:.2f} с !".format(team1_time))
+
+team1_num = 5
+team2_num = 6
+
+print("В команде Мастера кода участников: %s !" % team1_num)
+print("Итого сегодня в командах участников: %s и %s !" % (team1_num, team2_num))
